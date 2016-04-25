1b97947ecf19879117e1b9a5d818722f7836d6d9
Evan Stevenson <evanstevenson13@gmail.com>
2016-04-24 20:50:51 -0400

    Added test project

---
+++ HttpCommandTest/HttpCommand.cs
@@ -0,0 +1,13 @@
+﻿
+
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HttpCommandTest{
+    [TestClass]
+    public class HttpCommandTest{
+        [TestMethod]
+        public void TestMethod1(){
+        }
+    }
+}
